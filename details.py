ce7f8d580f3751a4ae2f8344150e88c28db10eb1
gunda_bala_kishore <141509871+karthikitgit@users.noreply.github.com>
2023-12-03 18:51:56 +0530

    Add files via upload

---
+++ details.py
@@ -0,0 +1,13 @@
+def print_details():
+    name = "GUNDA BALA KISHORE"
+    age = 23
+    mobile_number = "9515767950"
+    address = "37-135/1 JAGATHGIRI GUTTA"
+
+    print("NAME :", name)
+    print("AGE :", age)
+    print("MOBILE NUMBER :", mobile_number)
+    print("ADDRESS :", address)
+
+# Call the function to print the details
+print_details()
